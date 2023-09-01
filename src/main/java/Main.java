66e178512503c92a5b97a844b3e83d1cd9cc9d2e
MVe11111 <90624942+Sprudelflasch@users.noreply.github.com>
2023-09-01 11:12:52 +0200

    Step2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,15 +7,24 @@ public class Main {
 
         Student student1 = new Student("Max", "Mustermann", 12345);
         Student student2 = new Student("Anna", "Musterfrau", 67890);
+        Student student3 = new Student("Hubert", "Mustermann", 23424);
+        Student student4 = new Student("Gabrielle", "Musterfrau", 535636);
 
         studentenListe.add(student1);
         studentenListe.add(student2);
+        studentenListe.add(student3);
+        studentenListe.add(student4);
+
+        studentenListe.remove(student2);
 
         // Weitere Studenten hinzufügen, falls erforderlich
 
-        // Ausgabe der Studentenliste
+        // Ausgabe der Studentenliste über for each
         for (Student student : studentenListe) {
             System.out.println(student);
         }
+
+
     }
+
 }
